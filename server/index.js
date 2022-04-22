9a0ce6b4bd34fc325826fcf07837ea77a06b3a5b
hasan-ullah <mdhasanullah124@gmail.com>
2022-04-22 21:19:45 +0600

    database connected with server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config();
 const app = express();
@@ -8,6 +9,17 @@ const PORT = process.env.PORT || 5000;
 //middlewere 
 app.use(cors())
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gtyxm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('ginious cardb connected')
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/',(req,res)=>{
     res.send('home')
